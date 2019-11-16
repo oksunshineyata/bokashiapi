c4b1a5297cc7cec1b96305cc0049c9058aeb888a
--global <yatasunshine.1456@gmail.com>
2019-11-16 21:25:33 +0900

    decode and encode

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
 import responder
+import base64
 
 api = responder.API()
 
 
 @api.route("/")
-def index(req, resp):
-    resp.text = "Hello,World!"
+async def index(req, resp):
+
+    data = await req.media()
+    binary = data['image']
+    img = base64.b64decode(binary)
+
+    # face_recognition
+    ## TBD
+    # bokashi
+    bokashi = ''
+
+    img_base64 = base64.b64encode(bokashi)
+    resp.media = {"Image_base64": img_base64}
 
 
 if __name__ == '__main__':
