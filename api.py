67fe58db3698106514e7884fc9342baa55061c03
--global <yatasunshine.1456@gmail.com>
2019-11-17 12:25:32 +0900

    blur and paste

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 import responder
 import base64
+from PIL import Image, ImageFilter
+import face_recognition
+from io import BytesIO
 
 api = responder.API()
+img_path = './image.jpg'
 
 
 @api.route("/")
@@ -11,15 +15,62 @@ async def index(req, resp):
     binary = data['image']
     name = data['name']
     img = base64.b64decode(binary)
+    with open(img_path, 'wb') as f:
+        f.write(img)
 
     # face_recognition
-    ## TBD
-    # bokashi
-    # bokashi = ''
+    face_locations = get_face_locations(img_path)
+    faces = extract_faces(face_locations, img_path)
+    filtered_faces = gaussian_blur(faces)
+    blur_img = embed_filtered_faces(img_path, filtered_faces, face_locations)
 
-    img_base64 = base64.b64encode(img)
-    resp.media = {"image": img_base64, 'name': name}
+    buffered = BytesIO()
+    blur_img.save(buffered, format="JPEG")
+    img_base64 = base64.b64encode(buffered.getvalue())
+
+    img_utf_8 = img_base64.decode("utf-8")
+    if len(face_locations) == 0:
+        resp.media = {"image": img_utf_8, 'name': name, "status": "No detection"}
+    else:
+        resp.media = {"image": img_utf_8, 'name': name}
+
+
+def get_face_locations(img_path):
+    image = face_recognition.load_image_file(img_path)
+    face_locations = face_recognition.face_locations(image, model='cnn')
+    return face_locations
+
+
+def extract_faces(face_locations, img_path):
+    img = face_recognition.load_image_file(img_path)
+    faces = []
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        face_image = img[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        faces.append(pil_image)
+    return faces
+
+
+def gaussian_blur(faces):
+    filtered_faces = []
+    for face in faces:
+        filtered_face = face.filter(ImageFilter.GaussianBlur(4.0))
+        filtered_faces.append(filtered_face)
+
+    return filtered_faces
+
+
+def embed_filtered_faces(img_path, filtered_faces, face_locations):
+    img = face_recognition.load_image_file(img_path)
+    img = Image.fromarray(img)
+    for i in range(len(filtered_faces)):
+        face = filtered_faces[i]
+        top, _, _, left = face_locations[i]
+        img.paste(face, (left, top))
+
+    return img
 
 
 if __name__ == '__main__':
-    api.run(address='0.0.0.0')
+    api.run(address='0.0.0.0', debug=True)
