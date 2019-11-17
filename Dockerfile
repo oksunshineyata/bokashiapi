591eb112082c9841d6ae4a9d423c39b73bedff0c
--global <yatasunshine.1456@gmail.com>
2019-11-17 09:33:06 +0900

    reformatted json

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM python:3.7.5-stretch
 COPY . .
+RUN apt-get -y update
+RUN apt-get install -y --fix-missing \
+    build-essential \
+    cmake
 RUN  pip install -r requirements.txt
 CMD python api.py
--- api.py
+++ api.py
@@ -9,15 +9,16 @@ async def index(req, resp):
 
     data = await req.media()
     binary = data['image']
+    name = data['name']
     img = base64.b64decode(binary)
 
     # face_recognition
     ## TBD
     # bokashi
-    bokashi = ''
+    # bokashi = ''
 
-    img_base64 = base64.b64encode(bokashi)
-    resp.media = {"Image_base64": img_base64}
+    img_base64 = base64.b64encode(img)
+    resp.media = {"image": img_base64, 'name': name}
 
 
 if __name__ == '__main__':
